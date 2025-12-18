2ad77c89bc20087cb6adbac1033cb0b515eb934d
outla <iwo.ropa@gmail.com>
2025-12-18 20:05:58 +0100

    Removed generation of agents for all obstacle points

--- model.py
+++ model.py
@@ -61,7 +61,7 @@ class ColonyModel(mesa.Model):
         final_seed = get_value(seed)
         self.num_agents = int(get_value(N))
         self.uid = 0
-        self.scenario = "rock"
+        self.scenario = "rock" # possible values: base (no obstacles), rock (one rock generated), tunnel (nest inside a tunnel)
         self.g = float(get_value(g))
         self.J_11 = float(get_value(J_11))
         self.J_12 = float(get_value(J_12))
